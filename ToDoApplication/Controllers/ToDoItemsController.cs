5348f0008440664be8eebeb936b9a6241bfa8b09
Eduard Mariniuc <43582902+edwardedi@users.noreply.github.com>
2024-10-26 14:45:39 +0300

    Added add-to-do-item functionality

---
+++ ToDoApplication/Controllers/ToDoItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Use_Cases.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApplication.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ToDoItemsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateToDoItem(CreateToDoItemCommand command)
+        {
+            return await mediator.Send(command); 
+        }
+    }
+}
